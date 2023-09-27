f5f459f8f09e86441eeac900d3cf721e255af3ed
Lekhanapriya <21A91A04L2@aec.edu.in>
2023-09-27 21:35:16 +0530

    react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Main from './Components/Main';
+import { SomeComponent } from 'react';
+import './Components/style.css';
+
+function App() {
+  return (
+    <>
+      <Main/>
+    </>
+  );
+}
+
+export default App;
